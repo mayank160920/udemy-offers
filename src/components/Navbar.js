ff30cadb3d9332253f35b4ba6160f0c25a3f8952
mayank160920 <ay160920@gmail.com>
2021-09-13 22:52:30 +0530

    darkMode added, searchBox centered

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { lightTheme, darkTheme } from '../utils/Themes';
 import { NavLink } from 'react-router-dom';
-import { FaHome, FaInfoCircle } from 'react-icons/fa';
+import { FaHome, FaInfoCircle, FaSun, FaMoon } from 'react-icons/fa';
 
 function Navbar() {
-  const [title, settitle] = useState('Udemy Offers');
-
   const links = [
     { id: 0, title: 'Udemy Offers', icon: <FaHome />, path: '/' },
     { id: 1, title: 'About', icon: <FaInfoCircle />, path: '/about' }
   ];
+
+  const [title, settitle] = useState('Udemy Offers');
+  const [nightMode, setnightMode] = useState(false);
+
+  useEffect(() => {
+    for (const [key, value] of Object.entries(
+      nightMode ? darkTheme : lightTheme
+    )) {
+      document.querySelector(':root').style.setProperty(key, value);
+    }
+  }, [nightMode]);
 
   return (
     <div className="navBar">
@@ -26,6 +36,19 @@ function Navbar() {
             {link.icon}
           </NavLink>
         ))}
+        {nightMode ? (
+          <FaMoon
+            className="navItem"
+            style={{ color: 'orange' }}
+            onClick={() => setnightMode(!nightMode)}
+          />
+        ) : (
+          <FaSun
+            className="navItem"
+            style={{ color: 'orange' }}
+            onClick={() => setnightMode(!nightMode)}
+          />
+        )}
       </div>
     </div>
   );
---
+++ src/utils/Themes.js
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  '--bgColor': 'white',
+  '--textPrimaryColor': 'black',
+  '--navItemColor': 'gray',
+  '--navItemActiveColor': 'black',
+  '--offerCardBgColor': 'white',
+  '--spinnerColor': 'black'
+};
+
+export const darkTheme = {
+  '--bgColor': 'black',
+  '--textPrimaryColor': 'white',
+  '--navItemColor': 'gray',
+  '--navItemActiveColor': 'white',
+  '--offerCardBgColor': '#333',
+  '--spinnerColor': 'white'
+};
